86c8f88b9932e33470fe88a071f42ede9ae986f9
Chester Wicipto <63380942+ChesterWicipto@users.noreply.github.com>
2021-04-20 13:24:28 +0800

    second commit
    second commit

---
+++ App.js
@@ -0,0 +1,23 @@
+import "./App.css";
+import About from "./new/About";
+import Daftar from "./new/Daftar";
+import Dashboard from "./new/Dashboard";
+import Masuk from "./new/Masuk";
+import Routes from "./new/Pintas";
+
+
+const App = () => {
+    return(
+        <div className="Style">
+        {/* <Masuk/> */}
+        {/* <Daftar/> */}
+        {/* <Dashboard/> */}
+        {/* <About/> */}
+        <Routes/>
+        </div>
+    
+    );
+    
+}
+
+export default App;
